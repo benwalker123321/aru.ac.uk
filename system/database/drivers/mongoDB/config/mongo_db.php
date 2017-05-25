082b4eea25e05f7c4a722fc63aa84284a71004c6
benwalker123321 <ben1990sanjose@hotmail.co.uk>
2017-05-25 12:42:34 +0100

    Add files via upload

---
+++ system/database/drivers/mongoDB/config/mongo_db.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+	
+$config['mongo_host']            = 'localhost';
+$config['mongo_port']            = 80;
+$config['mongo_user']            = 'root';
+$config['mongo_pass']            = 'password';
+$config['mongo_db']              = 'aruMongoDB';
+$config['mongo_replicaset']      = FALSE;
+$config['mongo_slave_ok']        = TRUE;
+$config['mongo_write_timeout']   = 5000;
+$config['mongo_ensure_replicas'] = 0;
+$config['mongo_update_all']      = TRUE;
+$config['mongo_remove_all']      = TRUE;
+$config['mongo_use_upsert']      = TRUE;
+$config['mongo_expand_dbrefs']   = TRUE;
+
+
+
+
